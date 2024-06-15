0a0d4466b76d4de43f9f573f9da37d902777f352
miraj <mirajhadish48@gmail.com>
2024-06-15 08:00:45 +0530

    villa controller added

---
+++ MagicVilla_VillaAPI/Models/Villa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicVilla_VillaAPI.Models
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
