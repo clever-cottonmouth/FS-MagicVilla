4ca32cc7546bd0b380a5563556f640319b95970e
miraj <mirajhadish48@gmail.com>
2024-06-24 19:00:51 +0530

    utility added in sln

---
+++ MagicVilla_Web/Models/ApiRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static MagicVilla_Utility.SD;
+
+namespace MagicVilla_Web.Models
+{
+    public class ApiRequest
+    {
+        public ApiType ApiType { get; set; } = ApiType.GET;
+        public string Url { get; set; }
+        public object Data { get; set; }
+
+    }
+}
