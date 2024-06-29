5a77346509fb01851e1c00ac721f8f88dabe5fe5
miraj <mirajhadish48@gmail.com>
2024-06-29 22:40:33 +0530

    adding user repository

---
+++ MagicVilla_VillaAPI/Repository/IRepository/IUserRespository.cs
@@ -0,0 +1,17 @@
+﻿using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicVilla_VillaAPI.Repository.IRepository
+{
+    public interface IUserRespository
+    {
+        bool IsUniqueUser(string username);
+        Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto);
+        Task<LocalUser> Register(RegisterationRequestDto registerationRequestDto);
+    }
+}
---
+++ MagicVilla_VillaAPI/Repository/UserRepository.cs
@@ -0,0 +1,37 @@
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
+using MagicVilla_VillaAPI.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicVilla_VillaAPI.Repository
+{
+    public class UserRepository : IUserRespository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UserRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public bool IsUniqueUser(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LocalUser> Register(RegisterationRequestDto registerationRequestDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
