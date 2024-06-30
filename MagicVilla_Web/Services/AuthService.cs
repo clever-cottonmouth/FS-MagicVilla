0d89a9e98e5edde9ff83932f3759a5a795d4e2e9
miraj <mirajhadish48@gmail.com>
2024-06-30 08:04:00 +0530

    auth controller action method

---
+++ MagicVilla_Web/Services/AuthService.cs
@@ -0,0 +1,44 @@
+﻿using MagicVilla_Utility;
+using MagicVilla_Web.Models;
+using MagicVilla_Web.Models.Dto;
+using MagicVilla_Web.Services.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicVilla_Web.Services
+{
+    public class AuthService : BaseService, IAuthService
+    {
+        private readonly IHttpClientFactory _clientFactory;
+        private string villaUrl;
+
+        public AuthService(IHttpClientFactory clientFactory, IConfiguration configuration) :base(clientFactory)
+        {
+            _clientFactory = clientFactory;
+            villaUrl = configuration.GetValue<string>("ServiceUrls:VillaAPI");
+        }
+
+        public Task<T> LoginAsync<T>(LoginRequestDto objToCreate)
+        {
+            return SendAsync<T>(new ApiRequest()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = objToCreate,
+                Url = villaUrl + "/api/UsersAuth/login"
+            });
+        }
+
+        public Task<T> RegisterAsync<T>(RegisterationRequestDto objToCreate)
+        {
+            return SendAsync<T>(new ApiRequest()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = objToCreate,
+                Url = villaUrl + "/api/UsersAuth/register"
+            });
+        }
+    }
+}
---
+++ MagicVilla_Web/Services/IServices/IAuthService.cs
@@ -0,0 +1,15 @@
+﻿using MagicVilla_Web.Models.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicVilla_Web.Services.IServices
+{
+    public interface IAuthService
+    {
+        Task<T> LoginAsync<T>(LoginRequestDto objToCreate);
+        Task<T> RegisterAsync<T>(RegisterationRequestDto objToCreate);
+    }
+}
