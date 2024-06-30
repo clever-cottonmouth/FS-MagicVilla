7fca1c7c7b416067a5ea4a252b61f649909e41e9
miraj <mirajhadish48@gmail.com>
2024-06-30 07:44:21 +0530

    add dto for login and registeration

---
+++ MagicVilla_Web/Models/Dto/LoginResponseDto.cs
@@ -0,0 +1,15 @@
+﻿using MagicVilla_Web.Models.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicVilla_VillaAPI.Models.Dto
+{
+    public class LoginResponseDto
+    {
+        public UserDto User { get; set; }
+        public string Token { get; set; }
+    }
+}
