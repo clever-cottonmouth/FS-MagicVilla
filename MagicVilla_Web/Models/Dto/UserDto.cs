df6efc932e0abf1a825383db2fc22fa1fef55ba6
miraj <mirajhadish48@gmail.com>
2024-07-04 22:27:18 +0530

    updated role

--- MagicVilla_Web/Models/Dto/UserDto.cs
+++ MagicVilla_Web/Models/Dto/UserDto.cs
@@ -12,6 +12,5 @@ namespace MagicVilla_Web.Models.Dto
         public string UserName { get; set; }
         public string Name { get; set; }
         public string Password { get; set; }
-        public string Role { get; set; }
     }
 }
