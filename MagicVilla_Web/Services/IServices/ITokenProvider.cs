7d86ed779bfbadaf0cc1544491b85e25a3a6c03c
mirajhad <mirajhadish48@gmail.com>
2024-07-08 01:33:38 +0530

    created token provider

---
+++ MagicVilla_Web/Services/IServices/ITokenProvider.cs
@@ -0,0 +1,16 @@
+﻿using MagicVilla_VillaAPI.Models.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicVilla_Web.Services.IServices
+{
+    public interface ITokenProvider
+    {
+        void SetToken(TokenDto tokenDto);
+        TokenDto? GetToken();
+        void ClearToken();
+    }
+}
---
+++ MagicVilla_Web/Services/TokenProvider.cs
@@ -0,0 +1,28 @@
+﻿using MagicVilla_VillaAPI.Models.Dto;
+using MagicVilla_Web.Services.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicVilla_Web.Services
+{
+    public class TokenProvider : ITokenProvider
+    {
+        public void ClearToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TokenDto? GetToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetToken(TokenDto tokenDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
