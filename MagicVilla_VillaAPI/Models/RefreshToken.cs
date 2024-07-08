1ff0ff66b36a8adc51a4f994c3dda8a409b9afac
mirajhad <mirajhadish48@gmail.com>
2024-07-08 11:40:56 +0530

    refresh token

---
+++ MagicVilla_VillaAPI/Models/RefreshToken.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicVilla_VillaAPI.Models
+{
+    public class RefreshToken
+    {
+        [Key]
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string JwtTokenId { get; set; }
+        public string Refresh_Token { get; set; }
+        public bool IsValid { get; set; }
+        public DateTime ExpiresAt { get; set; }
+    }
+}
