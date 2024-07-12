3dc577abd28a1882288aeb8dc10f44acdb6c71cb
mirajhad <mirajhadish48@gmail.com>
2024-07-12 10:32:05 +0530

    apply pending migration

--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -103,4 +103,19 @@ app.MapControllers();
 
 app.UseDeveloperExceptionPage();
 
+ApplyMigration();
+
 app.Run();
+
+
+void ApplyMigration()
+{
+    using ( var scope = app.Services.CreateScope())
+    {
+        var _db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        if(_db.Database.GetPendingMigrations().Count() > 0)
+        {
+            _db.Database.Migrate();
+        }
+    }
+}
